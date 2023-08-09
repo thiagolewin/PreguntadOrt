7cb4aa84add5a997ff9e0e231577996c5fa207b5
THIAGO NICOLÁS LEWIN <47393575@ort>
2023-08-09 10:35:48 -0300

    Commit 1

---
+++ Models/BD.cs
@@ -0,0 +1,18 @@
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Dapper;
+public static class BD {
+    private static string _connectionString = @"Server=localhost;DataBase=PreguntadOrt;Trusted_Connection=True;";
+    public static void ObtenerCategorias() {
+
+    }
+    public static void ObtenerDificultades(){
+
+    }
+    public static ObtenerPreguntas(int dificultad, int categoria) {
+
+    }
+    public static ObtenerRespuestas(List<Pregunta> preguntas) {
+        
+    }
+}
